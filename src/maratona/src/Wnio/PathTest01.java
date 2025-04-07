d4fddae33089ad3b7761fd87aded76c4470992bf
Joyce Souto <140851285+joycsouto@users.noreply.github.com>
2025-04-07 16:45:34 -0300

    Path Tes01

---
+++ src/maratona/src/Wnio/PathTest01.java
@@ -0,0 +1,18 @@
+package Wnio;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathTest01 {
+    public static void main(String[] args) {
+
+        Path p1 = Paths.get("C:\\Users\\User\\IdeaProjects\\CursoJavaDevDojo\\src\\maratona\\File");
+        Path p2 = Paths.get("C:\\Users\\User\\IdeaProjects\\CursoJavaDevDojo\\src\\maratona","File");
+        Path p3 = Paths.get("C:\\","Users\\User\\IdeaProjects\\CursoJavaDevDojo\\src\\maratona\\File");
+        Path p4 = Paths.get("C:","Users","User","IdeaProjects","CursoJavaDevDojo","src","maratona","File");
+        System.out.println(p1.getFileName());
+        System.out.println(p2.getFileName());
+        System.out.println(p3.getFileName());
+        System.out.println(p4.getFileName());
+    }
+}
