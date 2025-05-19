cde92d4633744ae806636a4f341ecd67a5b572db
Joyce Souto <140851285+joycsouto@users.noreply.github.com>
2025-05-18 22:12:39 -0300

    feature: Aula 167 - Coleções pt 07 - List pt 02

--- src/maratona/src/VCollections/test/ListTest01.java
+++ src/maratona/src/VCollections/test/ListTest01.java
@@ -7,16 +7,27 @@ public class ListTest01
 {
     public static void main(String[] args) {
         List<String> nomes = new ArrayList<>(16);
+        List<String> nomes1 = new ArrayList<>(16);
         nomes.add("Joyce");
         nomes.add("Souto");
+
+        nomes1.addAll(nomes);
+        //nomes.remove("Joyce" );
+        for (String nome: nomes1){
+            System.out.println(nome);
+        }
+        System.out.println("---------");
         for (String nome: nomes){
             System.out.println(nome);
         }
         nomes.add("18");
+
         for (int i = 0; i < nomes.size(); i++) {
             System.out.println(nomes.get(i));
-
         }
+
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(12);
 
     }
 }
